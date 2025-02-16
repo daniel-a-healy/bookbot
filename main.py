f45913424b29e29038ad5b62807a7ca455a082b3
daniel-a-healy <daniel.addison.healy@gmail.com>
2025-02-15 22:00:34 -0500

    Added get_word_count() function

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+def get_word_count(file):
+    split_file = file.split()
+    return len(split_file)
+
 def main():
     path_to_file = "books/frankenstein.txt"
     with open(path_to_file) as f:
         file_contents = f.read()
     print(file_contents)
+    print(get_word_count(file_contents))
 
 main()
